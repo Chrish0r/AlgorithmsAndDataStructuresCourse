a5f77810c7f434225f703d6e4a847e8b25b591a5
Chrish0r <Christof.Gadek10@googlemail.com>
2020-09-14 14:33:45 +0200

    added example to data structure 'linked list'

---
+++ Datenstruktur/src/linkedList/LinkedListExample.java
@@ -0,0 +1,37 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+        // Hinzufügen
+        list.add("Peter Müller");
+        list.add("Laura");
+        list.add("Ralf");
+
+        list.addFirst("Chris");
+        list.addLast("Andreas");
+
+        for(String person : list) {
+            System.out.println(person);
+        }
+        System.out.println();
+        // Suchen
+
+        if(list.contains("Laura")) {
+            System.out.println("Gefunden!" + System.lineSeparator());
+        } else {
+            System.out.println("Nicht gefunden!" + System.lineSeparator());
+        }
+
+        // Löschen
+        list.remove("Laura");
+        for(String person : list) {
+            System.out.println(person);
+        }
+
+    }
+}
