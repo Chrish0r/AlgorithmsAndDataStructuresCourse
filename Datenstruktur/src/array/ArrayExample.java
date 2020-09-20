a89e9f0a59e8b19acd5f2dd8bd25ec3c5011936a
Chrish0r <Christof.Gadek10@googlemail.com>
2020-09-20 09:50:18 +0200

    test method

--- Datenstruktur/src/array/ArrayExample.java
+++ Datenstruktur/src/array/ArrayExample.java
@@ -64,4 +64,8 @@ public class ArrayExample {
             System.out.println(copyCustomizedRangeGreater[i] + System.lineSeparator());
         }
     }
+
+    public void test() {
+        
+    }
 }
