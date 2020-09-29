cd27985bb2d2c5b8fefa1508d87c8df781ace5a4
Chrish0r <Christof.Gadek10@googlemail.com>
2020-09-29 10:37:49 +0200

    start implementing data structure 'binary tree'

---
+++ Datenstruktur/src/binaryTree/Node.java
@@ -0,0 +1,23 @@
+package binaryTree;
+
+public class Node {
+    private int value;
+
+    private Node leftNode;
+    private Node rightNode;
+    private Node parentNode;
+
+    public Node(int value) {
+        this.value = value;
+        leftNode = null;
+        rightNode = null;
+    }
+
+    //---------------logic/methods--------------------------------------------------------
+        // specific binary tree methods
+
+
+
+
+    //--------------getters and setters-----------------------------------
+}
