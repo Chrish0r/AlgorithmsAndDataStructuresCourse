81ed373066610989f9c2e165bb1516e4356d0cdf
Chrish0r <Christof.Gadek10@googlemail.com>
2020-10-10 10:01:49 +0200

    completed implementation of example regarding selection sort algorithm

--- SortingAlgorithms/src/selectionSort/SelectionSortExample.java
+++ SortingAlgorithms/src/selectionSort/SelectionSortExample.java
@@ -2,7 +2,31 @@ package selectionSort;
 
 public class SelectionSortExample {
 
-    public int[] selectionSort(int[] arrayToSort) {
+    public static void main(String[] args) {
+
+        int[] unsortedArray = {16, 23, 14, 7, 21, 20, 6, 1, 17, 13, 12, 9, 3, 19};
+
+        System.out.println("Unsorted Array:");
+        for (int i = 0; i < unsortedArray.length; i++) {
+            if(i == unsortedArray.length - 1) {
+                System.out.println(unsortedArray[i] + System.lineSeparator());
+                break;
+            }
+            System.out.print(unsortedArray[i] + ", ");
+        }
+
+        int[] sortedArray = new SelectionSortExample().selectionSort(unsortedArray);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < sortedArray.length; i++) {
+            if(i == sortedArray.length - 1) {
+                System.out.println(sortedArray[i] + System.lineSeparator());
+                break;
+            }
+            System.out.print(sortedArray[i] + ", ");
+        }
+    }
+
+    public static int[] selectionSort(int[] arrayToSort) {
         for(int i = 0; i < arrayToSort.length - 1; i++) {
         //    int min = arrayToSort[i];
             for(int j = i + 1; j < arrayToSort.length; j++) {
