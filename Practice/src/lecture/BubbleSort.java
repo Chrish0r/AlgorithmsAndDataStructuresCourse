51aa2baa2af84528f06181bf6cfc55841edb5989
Chrish0r <Christof.Gadek10@googlemail.com>
2020-10-29 09:11:27 +0100

    alternative implementation of selection sort algorithm

--- Practice/src/lecture/BubbleSort.java
+++ Practice/src/lecture/BubbleSort.java
@@ -17,14 +17,17 @@ public class BubbleSort {
 
     // logic: the smallest value goes from end to start
     public static void bubbleSort(int arr[], int n) {
+        int swapCount = 0;
         for(int i = 0; i < n; i++) {
             for(int j = n-2; j >= i; j--) {
                 if(arr[j] > arr[j+1]) {
                     int h = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = h;
+                    swapCount++;
                 }
             }
         }
+        System.out.println("swap count is: " + swapCount);
     }
 }
---
+++ Practice/src/lecture/SelectionSort.java
@@ -0,0 +1,33 @@
+package lecture;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = {34, 45, 12, 34, 23, 18, 38, 17, 43, 51};
+
+        selectionSort(array, array.length);
+
+        for(int i = 0; i  < array.length; i++) {
+            if(i == array.length-1) {
+                System.out.println(array[i]);
+                break;
+            }
+            System.out.print(array[i] + ", ");
+        }
+    }
+
+    private static void selectionSort(int[] a, int n) {
+        int min;
+        for(int i = 0; i < n; i++) {
+            min = i;
+
+            for(int j = i; j < n; j++) { // innerhalb dieser inneren Schleife wir die Position des Mimiums ermittelt
+                if(a[j] < a[min]) {
+                    min = j; // neues min ist an Position j
+                }
+            }
+            int h = a[i];
+            a[i] = a[min];
+            a[min] = h;
+        }
+    }
+}
